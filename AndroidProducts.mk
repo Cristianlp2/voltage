9dc571bb272fec4e185369956793c15ac133ba2b
Cristianlp2 <99617966+Cristianlp2@users.noreply.github.com>
2023-03-04 03:42:40 -0300

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/voltage_rosemary.mk
+
+COMMON_LUNCH_CHOICES := \
+    voltage_rosemary-user \
+    voltage_rosemary-userdebug \
+    voltage_rosemary-eng
---
+++ voltage_rosemary.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/rosemary/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+
+
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+TARGET_WITH_GAPPS := TRUE
+
+PRODUCT_NAME := voltage_rosemary
+PRODUCT_DEVICE := rosemary
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10S
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rosemary-user 11 RP1A.200720.011 V12.5.16.0.RKLMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/rosemary_global/rosemary:11/RP1A.200720.011/V12.5.16.0.RKLMIXM:user/release-keys
